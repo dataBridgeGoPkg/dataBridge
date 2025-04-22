f9c831d6944ea205863f05a3a17878d42d0e012d
Atmadeep DAS <atmadeep.das@Mac-001.lan>
2025-04-22 13:02:18 +0200

    fix

--- models/database.go
+++ models/database.go
@@ -23,7 +23,6 @@ var DB *sql.DB
 func Init() {
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
-	fmt.Println(dsn)
 	var err error
 	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
